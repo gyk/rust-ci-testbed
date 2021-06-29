ee3400025212e1f10e4a3352294bb8b03de2d8c6
Yukun Guo <gyk.net@gmail.com>
2021-06-29 18:19:05 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum SortBy {
+    Order,
+    Title,
+    Created,
+    Updated,
+}
+
+impl Default for SortBy {
+    fn default() -> SortBy {
+        SortBy::Order
+    }
+}
+
+// Comment this out
+impl From<&str> for SortBy {
+    fn from(mode: &str) -> Self {
+        match mode {
+            "order" => Self::Order,
+            "title" => Self::Title,
+            "created" => Self::Created,
+            "updated" => Self::Updated,
+            _ => Self::default(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let x: SortBy = dbg!(serde_json::from_str("\"order\"")).unwrap();
+        assert_eq(x, SortBy::Order);
+    }
+}
